4d37121bfdc2ec9983ef7584d7d6c336fa94b7ec
Evan Wang <wangxevan@gmail.com>
2020-06-10 00:45:41 +0800

    update guess_password

--- guess_password.py
+++ guess_password.py
@@ -1,5 +1,5 @@
 password = "a123456"   # 初始密碼
-time = 2  # 次數
+time = 3  # 次數
 
 while True :
 	your_pass = input ('請輸入密碼：  ')
@@ -9,9 +9,9 @@ while True :
 		break
 
 	else :
+		time  = time - 1
 		print ('密碼錯誤！還有', time , '次機會')
-		time = time - 1
-		if time == -1 :
+		if time == 0 :
 			print ('登入失敗')
 			break
 
