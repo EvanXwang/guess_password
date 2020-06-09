cd2afb72ff88764981b56f74a310e057d7190c6f
Evan Wang <wangxevan@gmail.com>
2020-06-10 01:01:01 +0800

    change while time > 0

--- guess_password.py
+++ guess_password.py
@@ -1,19 +1,17 @@
 password = "a123456"   # 初始密碼
 time = 3  # 次數
 
-while True :
+while time > 0  :
 	your_pass = input ('請輸入密碼：  ')
 
 	if your_pass == password  :
 		print ('登入成功')
-		break
+		break  # 逃出迴圈
 
 	else :
 		time  = time - 1
 		print ('密碼錯誤！還有', time , '次機會')
-		if time == 0 :
-			print ('登入失敗')
-			break
+		
 
     
 
